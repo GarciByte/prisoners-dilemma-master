19812f936719015fd662e1c778d47d6529816566
Dmargarb <145496969+GarciByte@users.noreply.github.com>
2025-01-21 18:26:29 +0100

    agregar prisionero

--- Tester.java
+++ Tester.java
@@ -7,7 +7,7 @@ public class Tester
     public static void main(String[] args)
     {
         Game myGame = new Game();
-        //myGame.roundRobin(201);
+        myGame.roundRobin(201);
         myGame.giveDetails(201);
     }
 }
