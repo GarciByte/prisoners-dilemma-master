4456a744736a4f5897f6f7b9cfc66066a3c382fe
AGALMAD <118524298+AGALMAD@users.noreply.github.com>
2025-01-21 18:23:56 +0100

    Cresenciano

---
+++ Cresenciano.java
@@ -0,0 +1,45 @@
+public class Cresenciano extends Prisoner{
+
+    private int vecesTraicionado;
+
+    public Cresenciano(){
+        lastPlay = null;
+        name = "Crescenciano";
+        vecesTraicionado = 0;
+    }
+
+    @Override
+    Choice makePlay(Choice oppLastPlay) {
+
+
+        Choice play = null;
+        if (oppLastPlay == null)
+        {
+            play = Choice.COOPERATE;
+        }
+        else
+        {
+
+            if(oppLastPlay.equals(Choice.DEFECT)){
+                vecesTraicionado ++;
+            }
+
+            if(vecesTraicionado >= 2)
+                play = oppLastPlay;
+            else
+                play = Choice.COOPERATE;
+
+
+        }
+
+        return play;
+
+    }
+
+    @Override
+    void init() {
+        super.init();
+        lastPlay = null;
+    }
+
+}
